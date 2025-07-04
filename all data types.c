8648888c252a011f48efd4f806954a68439095f5
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2025-07-04 23:12:59 +0530

    Add files via upload

---
+++ all data types.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	char a[20]="Bhuvana";
+	char b='k';
+	int c=5;
+	long long int d=8989567460;
+	float e=1.345689;
+	double f=345678945;
+	printf("%s\n%c\n%d\n%lld\n%f\n%lf",a,b,c,d,e,f);
+	return 0;
+	
+	
+}
+
