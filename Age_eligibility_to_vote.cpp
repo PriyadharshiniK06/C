e4fb43ec7f62a8f7d72dc6529cb04b23d46c7544
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2025-07-05 15:22:20 +0530

    Add files via upload

---
+++ Age_eligibility_to_vote.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ 
+   int a;
+   printf("ENTER YOUR AGE:");
+   scanf("%d",&a);
+   if(a>=18 )
+   {
+   	printf("You are eligible to vote");
+   	}
+   	else
+   	{
+   		printf("You are not eligible to vote");
+   		
+	 }
+	 return 0;
+}
