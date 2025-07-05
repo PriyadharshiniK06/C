8cebb46add687e3aaefb6a19d6816123ed465187
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2025-07-05 15:44:55 +0530

    Update Modulus.cpp

--- Modulus.cpp
+++ Modulus.cpp
@@ -2,12 +2,10 @@
 int main()
 {
 	int a,b,c;
-		printf("Enter any integer :");
-	scanf("%d",&a);
-	for (b=1;b<=35;b++)
-	{
-		c=a%b;
-	    printf("%d\n",c);
-	}
+		printf("Enter the value of a and b integer :");
+	scanf("%d%d",&a,&b);
+	c=a%b;
+	printf("%d\n",c);
+	
 	return 0;
 }
